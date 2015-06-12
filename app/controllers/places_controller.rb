f5542f574f84b306fd03cd4eadb3296ce4c9713c
Dave Ekhaus <david_ekhaus@apple.com>
2015-06-12 15:58:06 -0700

    wip

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,13 +2,14 @@ class PlacesController < ApplicationController
   respond_to :json
 
   def index
-    if params[:_page]
-      page    = params[:_page].to_i
-      perPage = (params[:_perPage] || 25).to_i
-      @places = Place.all.page(page).per_page(perPage)
-    elsif params[:_start] && params[:_end]
-      page      = params[:_start].to_i + 1
-      perPage  = params[:_end].to_i - params[:_start].to_i
+    if params[:_page] || (params[:_start] && params[:_end])
+      if params[:_page]
+        page    = params[:_page].to_i
+        perPage = (params[:_perPage] || 25).to_i
+      else
+        page    = params[:_start].to_i + 1
+        perPage = params[:_end].to_i - params[:_start].to_i
+      end
       @places = Place.all.page(page).per_page(perPage)
     else
       @places = Place.all
